3687a3ac9a3d14580b6be7ed50e475716c44fa4c
rhencher <rhencher@gmail.com>
2022-12-10 10:04:39 -0500

    Subsetting abilities to Data tab

--- N-London-Housing-Costs/server.R
+++ N-London-Housing-Costs/server.R
@@ -1,14 +1,29 @@
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(lubridate)
 library(DT)
-library(readr)
 
 shinyServer(function(input, output, session) {
   
   data <- read_csv("/Users/rachelhencher/Downloads/PropertyData_prices_sold_prices_2022-12-09.csv")
+    data$Type <- as.factor(data$Type)
+    data$'New-build' <- as.factor(data$'New-build')
+    data$Tenure <- as.factor(data$Tenure)
+    data$Date <- as.Date(data$Date, format="%m/%d/%Y")
+    data$Lat <- round(data$Lat, digits = 4)
+    data$Lng <- round(data$Lng, digits = 4)
   
-  output$table <- renderDT({
-    data
+  data <- data %>%
+    rename("New_Build" = 'New-build', "Price_Paid" = 'Price paid', "Latitude" = Lat, "Longitude" = Lng)
+  
+  data$price_grouping <-
+    ifelse(data$Price_Paid >= 1125000, "£1,125,000 or more",
+           ifelse(data$Price_Paid >= 750000, "£750,000-£1,124,999",
+                  ifelse(data$Price_Paid >= 375000, "£375,000-£749,999", "Less than £375,000")))
+  
+  output$table <- renderDataTable({
+    data %>% filter(price_grouping == input$prices) %>% select(input$variables)
   })
   
   
--- N-London-Housing-Costs/ui.R
+++ N-London-Housing-Costs/ui.R
@@ -3,26 +3,32 @@ library(shinydashboard)
 library(DT)
 
 # 'About' page
-about_info = HTML("<p>This is a test.</p>")
+about_info = HTML("<p>This data comes from propertydata.co.uk and explores the housing market for the N13 postcode of London, UK. I was a resident in this postcode for four years and was never able to break into the housing market, so this data is of personal interest. The data spans five years and includes information on 836 homes sold in this postcode during this time.</p>")
 
-about_tab = tabItem("about",fluidRow(box(about_info, width=8)))
+about_tab = tabItem("about",fluidRow(box(about_info, width = 8)))
 
 
 # 'Data' page
-data_table = box(DTOutput("table"), width=8)
+                   
+variable_choices <- box(checkboxGroupInput("variables", "Select the variable(s) to display: ", choices = c("Date", "Address", "Postcode", "Type", "New_Build", "Tenure", "Bedrooms", "Latitude", "Longitude", "Price_Paid"), selected = c("Date", "Postcode", "Type", "Bedrooms", "Price_Paid")), width = 2)
 
-data_tab = tabItem("data", fluidRow(data_table))
+price_choices <- box(selectInput("prices", "Subset the data by price grouping:", choices = c("£1,125,000 or more", "£750,000-£1,124,999", "£375,000-£749,999", "Less than £375,000")), width = 3)
+
+data_table = box(dataTableOutput("table"), width = 10)
+
+data_tab = tabItem("data", fluidRow(variable_choices, price_choices, data_table))
 
 
 dashboardPage(
-  skin="yellow",
-  dashboardHeader(title="N13 Housing Prices"),
+  skin = "yellow",
+  dashboardHeader(title = "N13 Housing Prices"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("About", tabName="about"),
-      menuItem("Data", tabName="data"),
-      menuItem("Data Exploration", tabName="exploration"),
-      menuItem("Modeling", tabName="modeling")
+      menuItem("About", tabName = "about"),
+      menuItem("Data", tabName = "data"),
+      menuItem("Data Exploration", tabName = "exploration"),
+      menuItem("Modeling", tabName = "modeling"),
+      menuItem("Prediction", tabName = "prediction")
     )
   ),
   dashboardBody(
